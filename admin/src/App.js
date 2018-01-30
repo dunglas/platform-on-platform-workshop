6b429ab6aa7f92c78707a9517b533659d71e7995
alphonse <alphonse@platform.sh>
2018-01-30 21:06:50 +0100

    update nelmio + url api

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,7 +5,7 @@ console.log(platformVar);
 let entrypoint = Object.keys(platformVar).find(url => url.startsWith('https://api.'));
 
 if(!entrypoint) {
-    entrypoint = 'https://api---master-7rqtwti-kpfnplkpyfk2k.eu.platform.sh/';
+    entrypoint = 'https://api.master-7rqtwti-7lhzn2k5xkll2.eu.platform.sh/';
 }
 entrypoint = entrypoint.substring(0, entrypoint.length - 1);
 console.log(entrypoint);
